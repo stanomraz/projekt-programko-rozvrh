75aff04f92aca6d4fef1ca3be2211a615116d2b2
stanomraz <rechtphl@gmail.com>
2024-11-22 23:44:09 +0100

    pridane path pre streamreader

--- projekt programko/Program.cs
+++ projekt programko/Program.cs
@@ -1,7 +1,7 @@
-﻿StreamReader ATrozvrhReader = new StreamReader("");
-StreamReader AIrozvrhReader = new StreamReader("");
-StreamReader AMrozvrhReader = new StreamReader("");
-StreamReader AErozvrhReader = new StreamReader("");
+﻿StreamReader ATrozvrhReader = new StreamReader(@"/Users/rechtphl/Documents/skola spsit/remis programko/projekt programko/projekt-programko-rozvrh/projekt programko");
+StreamReader AIrozvrhReader = new StreamReader(@"/Users/rechtphl/Documents/skola spsit/remis programko/projekt programko/projekt-programko-rozvrh/projekt programko");
+StreamReader AMrozvrhReader = new StreamReader(@"/Users/rechtphl/Documents/skola spsit/remis programko/projekt programko/projekt-programko-rozvrh/projekt programko");
+StreamReader AErozvrhReader = new StreamReader(@"/Users/rechtphl/Documents/skola spsit/remis programko/projekt programko/projekt-programko-rozvrh/projekt programko");
 
 //zistit ako funguje streamreader
 
@@ -20,6 +20,9 @@ while (true)
     string zvolenyDen = Console.ReadLine();
     Console.WriteLine("Zadaj hodinu (1-7):");
     int zvolenaHodina = Convert.ToInt32(Console.ReadLine());
+
+
+    //s ifkami spravit pristup ku konkretnym datam
 
 
 
