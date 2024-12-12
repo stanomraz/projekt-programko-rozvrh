cb01aaebbac23310d2ead90b4be21c9c90980c74
stanomraz <rechtphl@gmail.com>
2024-12-12 20:39:50 +0100

    final-copy
    konecne som skopiroval kod z win kompu na mac a pushol to na git

--- projekt programko/Program.cs
+++ projekt programko/Program.cs
@@ -1,20 +1,33 @@
-﻿StreamReader ATrozvrhReader = new StreamReader(@"/Users/rechtphl/Documents/skola spsit/remis programko/projekt programko/projekt-programko-rozvrh/projekt programko/rozvrhAT.txt");
+﻿//nacitanie rozvrhu z textaku
+StreamReader ATrozvrhReader = new StreamReader(@"/Users/rechtphl/Documents/skola spsit/remis programko/projekt programko/projekt-programko-rozvrh/projekt programko/rozvrhAT.txt");
 StreamReader AIrozvrhReader = new StreamReader(@"/Users/rechtphl/Documents/skola spsit/remis programko/projekt programko/projekt-programko-rozvrh/projekt programko/rozvrhAI.txt");
 StreamReader AMrozvrhReader = new StreamReader(@"/Users/rechtphl/Documents/skola spsit/remis programko/projekt programko/projekt-programko-rozvrh/projekt programko/rozvrhAM.txt");
 StreamReader AErozvrhReader = new StreamReader(@"/Users/rechtphl/Documents/skola spsit/remis programko/projekt programko/projekt-programko-rozvrh/projekt programko/rozvrhAE.txt");
 
+//inicializovanie poli listov stringov
+List<string>[] ATrozvrh = new List<string>[5];
+List<string>[] AIrozvrh = new List<string>[5];
+List<string>[] AMrozvrh = new List<string>[5];
+List<string>[] AErozvrh = new List<string>[5];
 
+//pridanie listu stringov do indexu pola
+for (int i = 0; i < ATrozvrh.Length; i++)
+    ATrozvrh[i] = new List<string>();
 
-List<List<string>> ATrozvrh = new List<List<string>>();
-List<List<string>> AIrozvrh = new List<List<string>>();
-List<List<string>> AMrozvrh = new List<List<string>>();
-List<List<string>> AErozvrh = new List<List<string>>();
+for (int i = 0; i < AIrozvrh.Length; i++)
+    AIrozvrh[i] = new List<string>();
 
+for (int i = 0; i < AMrozvrh.Length; i++)
+    AMrozvrh[i] = new List<string>();
 
+for (int i = 0; i < AErozvrh.Length; i++)
+    AErozvrh[i] = new List<string>();
+
+//precitanie suborov a ulozenie do listov
+int riadok = 0;
 while (true)
 {
     string obsah = ATrozvrhReader.ReadLine();
-    int riadok = 0;
 
 
 
@@ -27,25 +40,18 @@ while (true)
 
     for (int i = 0; i < poleRiadku.Length; i++)
     {
-        if (ATrozvrh.Count <= riadok)
-        {
-            ATrozvrh.Add(new List<string>());
-        }
-        while (ATrozvrh[riadok].Count <= i)
-        {
-            ATrozvrh[riadok].Add("");
-        }
-        ATrozvrh[riadok][i] = poleRiadku[i];
+        ATrozvrh[riadok].Add(poleRiadku[i]);
     }
 
     riadok++;
 }
 
-/*
+riadok = 0;
 while (true)
 {
     string obsah = AIrozvrhReader.ReadLine();
-    int riadok = 0;
+
+
 
     if (obsah == null)
     {
@@ -53,18 +59,21 @@ while (true)
     }
 
     string[] poleRiadku = obsah.Split(';');
+
     for (int i = 0; i < poleRiadku.Length; i++)
     {
-        AIrozvrh[riadok][i] = poleRiadku[i];
+        AIrozvrh[riadok].Add(poleRiadku[i]);
     }
 
     riadok++;
 }
 
+riadok = 0;
 while (true)
 {
     string obsah = AMrozvrhReader.ReadLine();
-    int riadok = 0;
+
+
 
     if (obsah == null)
     {
@@ -72,18 +81,21 @@ while (true)
     }
 
     string[] poleRiadku = obsah.Split(';');
+
     for (int i = 0; i < poleRiadku.Length; i++)
     {
-        AMrozvrh[riadok][i] = poleRiadku[i];
+        AMrozvrh[riadok].Add(poleRiadku[i]);
     }
 
     riadok++;
 }
 
+riadok = 0;
 while (true)
 {
     string obsah = AErozvrhReader.ReadLine();
-    int riadok = 0;
+
+
 
     if (obsah == null)
     {
@@ -91,37 +103,110 @@ while (true)
     }
 
     string[] poleRiadku = obsah.Split(';');
+
     for (int i = 0; i < poleRiadku.Length; i++)
     {
-        AErozvrh[riadok][i] = poleRiadku[i];
+        AErozvrh[riadok].Add(poleRiadku[i]);
     }
 
     riadok++;
 }
-*/
 
+//koniec prace so streamreaderom
 ATrozvrhReader.Close();
 AIrozvrhReader.Close();
 AMrozvrhReader.Close();
 AErozvrhReader.Close();
 
-
+//program s vypisom
 while (true)
 {
     Console.WriteLine("Zadaj triedu (at/ai/am/ae):");
     string zvolenaTrieda = Console.ReadLine();
     Console.WriteLine("Zadaj den (po/ut/st/št/pi):");
     string zvolenyDen = Console.ReadLine();
-    Console.WriteLine("Zadaj hodinu (1-7):");
+    Console.WriteLine("Zadaj hodinu (1-8):");
     int zvolenaHodina = Convert.ToInt32(Console.ReadLine());
+    int indexDna = 0;
 
-    for (int i = 0; i < ATrozvrh.Count; i++)
+    if (zvolenyDen == "po")
     {
-        Console.WriteLine(ATrozvrh[i]);
+        indexDna = 0;
+    }
+    else if (zvolenyDen == "ut")
+    {
+        indexDna = 1;
+    }
+    else if (zvolenyDen == "st")
+    {
+        indexDna = 2;
+    }
+    else if (zvolenyDen == "št")
+    {
+        indexDna = 3;
+    }
+    else if (zvolenyDen == "pi")
+    {
+        indexDna = 4;
     }
 
-    //s ifkami spravit pristup ku konkretnym datam
 
+    if (zvolenaTrieda == "at")
+    {
+        for (int i = 0; i < ATrozvrh.Length; i++)
+        {
+            string output = "";
+            for (int j = 0; j < ATrozvrh[i].Count; j++)
+            {
+                output += ATrozvrh[i][j] + ";";
+            }
+            Console.WriteLine(output.TrimEnd(';'));
+        }
+        Console.WriteLine();
+        Console.WriteLine(ATrozvrh[indexDna][zvolenaHodina - 1]);
+    }
+    else if (zvolenaTrieda == "ai")
+    {
+        for (int i = 0; i < AIrozvrh.Length; i++)
+        {
+            string output = "";
+            for (int j = 0; j < AIrozvrh[i].Count; j++)
+            {
+                output += AIrozvrh[i][j] + ";";
+            }
+            Console.WriteLine(output.TrimEnd(';'));
+        }
+        Console.WriteLine();
+        Console.WriteLine(AIrozvrh[indexDna][zvolenaHodina - 1]);
+    }
+    else if (zvolenaTrieda == "am")
+    {
+        for (int i = 0; i < AMrozvrh.Length; i++)
+        {
+            string output = "";
+            for (int j = 0; j < AMrozvrh[i].Count; j++)
+            {
+                output += AMrozvrh[i][j] + ";";
+            }
+            Console.WriteLine(output.TrimEnd(';'));
+        }
+        Console.WriteLine();
+        Console.WriteLine(AMrozvrh[indexDna][zvolenaHodina - 1]);
+    }
+    else if (zvolenaTrieda == "ae")
+    {
+        for (int i = 0; i < AErozvrh.Length; i++)
+        {
+            string output = "";
+            for (int j = 0; j < AErozvrh[i].Count; j++)
+            {
+                output += AErozvrh[i][j] + ";";
+            }
+            Console.WriteLine(output.TrimEnd(';'));
+        }
+        Console.WriteLine();
+        Console.WriteLine(AErozvrh[indexDna][zvolenaHodina - 1]);
+    }
 
     Console.ReadKey();
 }
